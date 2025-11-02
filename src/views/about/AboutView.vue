e1d8a04639c18e7d8716e77b23243dc0a14192e5
nick4134409042 <yanzexuan@163.com>
2025-11-02 12:14:56 +0000

    Merge #9 into develop from feature/about
    Modify about page
    
    * feature/about: (1 commits squashed)
    
      - Modify about page
    
    Signed-off-by: yanzexuan <yanzexuan@163.com>
    Merged-by: yanzexuan <yanzexuan@163.com>
    
    CR-link: https://codeup.aliyun.com/68c03aa6ddcdb4ded6028c72/panda/scene-editor/change/9

--- src/views/about/AboutView.vue
+++ src/views/about/AboutView.vue
@@ -1,15 +1,23 @@
 <template>
   <page-wrapper title="About" :content="description">
-    <project-info class="enter-y" />
+    <!-- <project-info class="enter-y" />
     <dependencies class="enter-y !mt-16px" />
-    <dev-dependencies class="enter-y !mt-16px" />
+    <dev-dependencies class="enter-y !mt-16px" /> -->
+      <div class="enter-y !mt-16px about-contact">
+        <h3>Contact</h3>
+        <ul>
+          <li>Website: <a href="https://www.thingraph.site/#/" target="_blank" rel="noopener noreferrer">https://www.thingraph.site/#/</a></li>
+          <li>GitHub: <a href="https://github.com/thingraph" target="_blank" rel="noopener noreferrer">https://github.com/thingraph</a></li>
+          <li>Email: <a href="mailto:thingraph@outlook.com">thingraph@outlook.com</a></li>
+        </ul>
+      </div>
   </page-wrapper>
 </template>
 
 <script setup lang="ts">
-import ProjectInfo from './components/ProjectInfo.vue'
-import Dependencies from './components/DependenciesInfo.vue'
-import DevDependencies from './components/DevDependencies.vue'
+// import ProjectInfo from './components/ProjectInfo.vue'
+// import Dependencies from './components/DependenciesInfo.vue'
+// import DevDependencies from './components/DevDependencies.vue'
 import { PageWrapper } from '@/components/Page'
 
 const { pkg } = __APP_INFO__
